832f2e693f4a23772d83a268291fabd653b7cb4b
Timoshenko <softctrl@users.noreply.github.com>
2017-09-27 15:40:31 -0300

    Create util.go

---
+++ util.go
@@ -0,0 +1,26 @@
+//
+// Author:
+//  Carlos Timoshenko
+//  carlostimoshenkorodrigueslopes@gmail.com
+//
+//  https://github.com/softctrl
+//
+// This project is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+package main
+
+import m "math"
+
+// DegreeToRadian converts any informed degree value to a correspondent in
+// radians.
+func DegreeToRadian(value float64) float64 {
+	return value * m.Pi / 180
+}
