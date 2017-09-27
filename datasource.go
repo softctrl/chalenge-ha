ebb30fdaabc13621e69b137c9eb317abfda3fc00
Timoshenko <softctrl@users.noreply.github.com>
2017-09-27 20:21:27 -0300

    Create datasource.go

---
+++ datasource.go
@@ -0,0 +1,24 @@
+//
+// Author:
+//  Carlos Timoshenko
+//  carlostimoshenkorodrigueslopes@gmail.com
+//
+//  https://github.com/softctrl
+//
+// This project is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+package main
+
+// Base Datasource. If you want to create a new source for data you only need
+// to implement this datasource under those terms:
+type DataSource interface {
+	Next() (l *Location, err error) // Next move to the next register if it exists.
+}
