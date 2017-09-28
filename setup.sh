fcc010af5ce2f064ca1066af0c0d9626ed7ce99f
Timoshenko <softctrl@users.noreply.github.com>
2017-09-27 21:54:26 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 
 cd && \
 wget -c https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz && \
-tar -zxvf go1.9.linux-amd64.tar.gz && PATH="${PATH}:/home/ubuntu/go/bin" && \
+tar -zxvf go1.9.linux-amd64.tar.gz && PATH="${PATH}:~/go/bin" && \
 mkdir Chalenge && cd Chalenge && cp -R /vagrant/* . && \
 go test -v && go build -o Chalenge
