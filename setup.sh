27d5f651432eeb7db956705a427c881207294ed3
Timoshenko <softctrl@users.noreply.github.com>
2017-09-27 21:53:47 -0300

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd && \
+wget -c https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz && \
+tar -zxvf go1.9.linux-amd64.tar.gz && PATH="${PATH}:/home/ubuntu/go/bin" && \
+mkdir Chalenge && cd Chalenge && cp -R /vagrant/* . && \
+go test -v && go build -o Chalenge
